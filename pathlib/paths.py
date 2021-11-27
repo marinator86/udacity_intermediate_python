f2564cd88d2e6be162ff2b77cda965f7b71a7d01
Mario Hammer <mario.hammer@breuninger.de>
2021-11-27 11:15:30 +0100

    file system

---
+++ pathlib/paths.py
@@ -0,0 +1,14 @@
+import collections
+import operator
+
+def count_unique_words(filename):
+    # your code here
+    result = collections.defaultdict(lambda: 1)
+    with open(filename, "r") as f:
+        t = f.read()
+        for w in t.split():
+            result[w] = result[w] + 1
+    return sorted(result.items(), key = operator.itemgetter(1), reverse = True)
+
+if __name__ == '__main__':
+    print(*count_unique_words('hamlet.txt')[:10], sep="\n")
