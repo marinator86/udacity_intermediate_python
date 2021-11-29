142ae4bda366d842c6043449386d00f835a71cdf
Mario Hammer <mario.hammer@breuninger.de>
2021-11-29 13:03:39 +0100

    added extractions

--- neos/extract.py
+++ neos/extract.py
@@ -14,7 +14,6 @@ You'll edit this file in Task 2.1
 """
 import csv
 import json
-
 from models import NearEarthObject, CloseApproach
 
 
@@ -24,9 +23,18 @@ def load_neos(neo_csv_path):
     :param neo_csv_path: A path to a CSV file containing data about near-Earth objects.
     :return: A collection of `NearEarthObject`s.
     """
-    # TODO: Load NEO data from the given CSV file.
-    # Use csv dict reader?
-    return ()
+    result = []
+    with open(neo_csv_path) as neo_file:
+        reader = csv.DictReader(neo_file)
+        for line in reader:
+            result.append(NearEarthObject(
+                line['pdes'],
+                line['name'] if line['name'] else None,
+                float(line['diameter']) if line['diameter'] else float('nan'),
+                line['pha'] == 'Y',
+            ))
+    print(f"Loaded {len(result)} NEOs")
+    return result
 
 
 def load_approaches(cad_json_path):
@@ -35,5 +43,42 @@ def load_approaches(cad_json_path):
     :param neo_csv_path: A path to a JSON file containing data about close approaches.
     :return: A collection of `CloseApproach`es.
     """
-    # TODO: Load close approach data from the given JSON file.
-    return ()
+    result = []
+    with open(cad_json_path) as cad_file:
+        cad_json = json.load(cad_file)
+        total = len(cad_json['data'])
+        print(f"Loading {total} CloseApproaches:")
+        current = 0
+        for cad in cad_json['data']:
+            result.append(CloseApproach(
+                designation=cad[0],
+                time=cad[3],
+                distance=float(cad[4]),
+                velocity=float(cad[7]),
+            ))
+            printProgressBar(current, total)
+            current += 1
+    print("Done.")
+    return result
+
+# https://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total:
+        print()
